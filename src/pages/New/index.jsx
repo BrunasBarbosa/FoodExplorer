5f7d43d2bdee0f7112e91ba7fcb4e7288eff1739
Bruna Barbosa <brunas_barbosa@hotmail.com>
2023-05-27 19:36:52 -0300

    create main new dish page

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,13 +1,18 @@
 import { HeaderAdm } from '../../components/HeaderAdm';
+import { Return } from '../../components/Return';
+import { Textarea } from '../../components/Textarea';
 import { Footer } from '../../components/Footer';
 
-import { Container } from './styles';
+import { Container, Main } from './styles';
 
 export function New() {
   return (
     <Container >
       <HeaderAdm />
-      
+      <Main >
+        <Return />
+        <h1>Novo prato</h1>
+      </Main>
     </Container>
-  )
+  );
 }
--- src/pages/New/styles.js
+++ src/pages/New/styles.js
@@ -3,3 +3,26 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
 `;
+
+export const Main = styled.main`
+  width: 100%;
+  padding: 1rem 3.2rem 5.3rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+
+  div:first-child {
+    font-size: 1.6rem;
+
+    svg {
+      height: 1.6rem;
+      width: 1.6rem;
+    }
+  }
+
+  h1 {
+    font-size: 3.2rem;
+    font-weight: 500;
+  }
+`;
